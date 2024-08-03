c500bdd77e89a436d103ebb93688d5ebe210f04f
Fuad Hasan <fuadgit@gmail.com>
2024-08-03 13:58:45 +0600

    Update big_to_small_number.c
    /*
    Problem is~
        
        Firstly, input who much number run this code. And then print number 1000 to 1 and 5 number in 1 line with single tap gap between two number
    */

--- C/big_to_small_number.c
+++ C/big_to_small_number.c
@@ -1,9 +1,3 @@
-/*
-Problem is~
-    
-    Firstly, input who much number run this code. And then print number 1000 to 1 and 5 number in 1 line with single tap gap between two number
-*/
-
 #include<stdio.h>
 
 int main(){
