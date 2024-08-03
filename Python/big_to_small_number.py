7dad233b85d22ab7e4f8fc011f2169b90f3b751b
Fuad Hasan <fuadgit@gmail.com>
2024-08-03 14:08:02 +0600

    Add files via upload

---
+++ Python/big_to_small_number.py
@@ -0,0 +1,10 @@
+/*
+Problem is~
+    
+    Firstly, input who much number run this code. And then print number 1000 to 1 and 5 number in 1 line with single tap gap between two number
+*/
+
+for i in range(1000, 0, -1):
+    if i % 5 == 0:
+        print()
+    print(i, end="\t")
